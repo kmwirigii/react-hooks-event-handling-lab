e7f027efa2eae21833b7d9fd93d1c32dbc0bdc09
Kelvin Mwirigi <kmwirigi05@gmail.com>
2025-04-16 10:08:46 +0300

    adds files

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
 // Code Keypad Component Here
-
-function Keypad (){
+import React from "react";
+function Keypad (){ 
+    const handleChange = () => {
+    console.log('Entering password...');
+  };
     return (
-        <div></div>
-    )
+        <div>
+            <input
+            type="password"
+            onChange={handleChange}
+            />
+        </div>
+    );
 }
 
 export default Keypad;
